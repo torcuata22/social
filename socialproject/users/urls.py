0cf7ea4e4f7e94fcd8cf44a768c874017fd033da
Marilyn Marquez <marilynmarquez@MMarquez.local>
2022-11-17 13:50:12 -0600

    reset password

--- socialproject/users/urls.py
+++ socialproject/users/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('logout/', auth_view.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('password_change/', auth_view.PasswordChangeView.as_view(template_name="users/password_change_form.html"), name='password_change'),
     path('password_change/done', auth_view.PasswordChangeDoneView.as_view(template_name="users/password_change_done.html"), name='password_change_done'),
+    path('password_reset/', auth_view.PasswordResetView.as_view(template_name='users/password_reset_form.html'), name='password-reset'), 
     ]
