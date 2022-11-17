a0bc084ef41f7169a161e299ed0850d672c90cb7
Marilyn Marquez <marilynmarquez@MMarquez.local>
2022-11-17 14:13:33 -0600

    password change confirm form not generating message or link to terminal

--- socialproject/socialproject/settings.py
+++ socialproject/socialproject/settings.py
@@ -124,4 +124,4 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend' #logs password reset link into console
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend' #logs password reset link into the terminal, not working
--- socialproject/users/urls.py
+++ socialproject/users/urls.py
@@ -11,4 +11,6 @@ urlpatterns = [
     path('password_change/', auth_view.PasswordChangeView.as_view(template_name="users/password_change_form.html"), name='password_change'),
     path('password_change/done', auth_view.PasswordChangeDoneView.as_view(template_name="users/password_change_done.html"), name='password_change_done'),
     path('password_reset/', auth_view.PasswordResetView.as_view(template_name='users/password_reset_form.html'), name='password-reset'), 
+    path('password_reset/done/', auth_view.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>',auth_view.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'),name='password_reset_confirm')
     ]
