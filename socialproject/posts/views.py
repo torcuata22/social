2b019e213da5f86e8d50c8e5ba6c046580d16f49
Marilyn Marquez <marilynmarquez@MMarquez.local>
2022-11-19 15:45:41 -0600

    form to create post and view

--- socialproject/posts/views.py
+++ socialproject/posts/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .forms import PostCreateForm
 
 # Create your views here.
+def post_create(request):
+    if request.method == 'POST':
+        form = PostCreateForm(data=request.POST)
+        if form.is_valid():
+            new_item = form.save(commit_to = False) #won't commit yet
+            new_item.user = request.user
+            new_item.save()
+    else:
+        form = PostCreateForm(data=request.GET)
+
+    return render(request, 'post/create.html', {'form' : form})
