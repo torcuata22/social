b80a40aa22f7529d5fd13fd017cc834067289d0d
Marilyn Marquez <marilynmarquez@MMarquez.local>
2022-11-19 16:14:44 -0600

    created posts

---
+++ socialproject/posts/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+from django.contrib.auth import views as auth_view
+
+
+urlpatterns = [
+    path('create', views.post_create, name='create'),
+    ]
