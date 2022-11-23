127df6984edb965c9f9b1d0741ba757edad423c9
Marilyn Marquez <marilynmarquez@MMarquez.local>
2022-11-23 15:01:40 -0600

    Ajax problem with Like functionality

--- socialproject/posts/views.py
+++ socialproject/posts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .forms import PostCreateForm
 from django.contrib.auth.decorators import login_required
 from .models import Post
+from django.shortcuts import get_object_or_404, redirect
 
 # Create your views here.
 
@@ -22,5 +23,16 @@ def post_create(request):
 def feed(request):
     posts = Post.objects.all()
     return render(request, 'posts/feed.html',{'posts':posts})
+
+def like_post(request):
+    post_id = request.post.get('post_id')
+    post = get_object_or_404(Post, id=post_id)
+    if post.liked_by.filter(id=request.user.id).exitst(): #post already been liked
+        post.liked_by.remove(request.user)
+    else: #post has not been liked
+        post.liked_by.add(request.user)
+
+    return redirect('feed')
+
 
     
